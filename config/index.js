920310451e0af0b47693154e6d35fe2f7f01a0f0
tanchidemao-147 <66361712+tanchidemao-147@users.noreply.github.com>
2022-08-31 23:20:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx7e45e2e396be7df1",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "d927c4383da1b47671bea49ac2feb8fb",
 
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    TEMPLATE_ID: "ptglh-LsL4v1OMJsItiOJVXeLzv_4-80TfO0NNHVohk",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
@@ -22,17 +22,14 @@ export const config = {
     // },
     // {
     //   name: "李四",
-    //   id: "wxafasdasdd-zxfvsdfd"
+    //   id: "oTg2w6vNqAt0NhB2mp3tRFpWmjts"
     // }]
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
+      {name: '艳艳', id: "oTg2w6vNqAt0NhB2mp3tRFpWmjts"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+      {name: '自己',id: "oTg2w6gaxXYtJE5k9wVGfNEJ9nMw"}, 
     ],
      
     /**
@@ -42,9 +39,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "上海",
     // 所在城市
-    CITY: "惠州",
+    CITY: "上海",
 
     /** 重要节日相关 */
 
@@ -53,10 +50,9 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+      {"type": "生日", "name": "宝宝的农历", "year": "1999", "date": "04-16"},
+      {"type": "生日", "name": "宝宝的阳历", "year": "1999", "date": "05-30"},
+      {"type": "节日", "name": "2周年纪念日", "year": "2022", "date": "10-25"},
     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
@@ -66,7 +62,7 @@ export const config = {
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
+    LOVE_DATE: "2020-10-20",
     // 结婚纪念日
     MARRY_DATE: "2020-01-04",
 
